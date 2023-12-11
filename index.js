5da41d1975fc2b463a805faf8ab3cd813899708b
marce1215 <marcelo.e.arriagada.m@gmail.com>
2023-12-11 11:19:56 -0300

    https://github.com/Marce1215/PF_backend.git

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+
 const jwt = require("jsonwebtoken")
-const cors = requiere('cors')
+const cors = require('cors')
 const { getPpto, IngItem, consultaPresupuesto, verificarCredenciales, ActualizarEvento, registrarUsuario } = require('./consultas')
 
-app.listen(4500, console.log("SERVER ON"))
+app.listen(4600, console.log("SERVER ON"))
 app.use(cors())
 app.use(express.json())
 app.use(cors());
 
 const { Pool } = require('pg')
-
-/*
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'marce1215',
-    database: 'presupuesto',
-    port: 5432, // El puerto predeterminado de PostgreSQL es 5432.
-    allowExitOnIdle: true
-})
-
-*/
-
 
 app.get("/ppto", async (req, res) => {
     try {
@@ -41,11 +28,11 @@ app.post("/usuarios", async (req, res) => {
         const usuario = req.body
         await registrarUsuario(usuario)
         res.send("Usuario creado con éxito")
-    } catch (error) {
+    } catch (error) { 
         res.status(500).send(error)
     }
 })
-
+/*
 app.post('/ingitem', async (req, res) => {
     try {
       //  const { id } = req.params;
@@ -63,7 +50,40 @@ app.post('/ingitem', async (req, res) => {
         const { id_ppto_ingreso, fecha, monto_item } = req.body;
 
         // Llamar a la función IngItem con los datos proporcionados
-        await IngItem(id_ppto_ingreso, fecha, monto_item);
+        await IngItem(id_ppto_ingreso, fecha, monto_item); 
+
+        res.status(200).send(`Presupuesto actualizado correctamente`);
+    } catch (error) {
+        console.error('Error al procesar la solicitud:', error);
+
+        if (error.code === 404) {
+            res.status(404).send(error.message);
+        } else if (error.code === 401) {
+            res.status(401).send(error.message);
+        } else {
+            res.status(error.code || 500).send(error.message || 'Error interno del servidor');
+        }
+    }
+});*/
+
+app.post('/ingitem', async (req, res) => {
+    try {
+      //  const { id } = req.params;
+//        const authorizationHeader = req.header("Authorization");
+
+  //      if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+    //        throw { code: 401, message: "Unauthorized: Token not provided" };
+      //  }
+
+        //const token = authorizationHeader.split("Bearer ")[1];
+
+        // Verificar y decodificar el token
+    //    const decodedToken = await jwt.verify(token, "az_AZ");
+
+        const { id_ppto_ingreso, fecha, monto_item, descripcion } = req.body;
+
+        // Llamar a la función IngItem con los datos proporcionados
+        await IngItem(id_ppto_ingreso, fecha, monto_item, descripcion); 
 
         res.status(200).send(`Presupuesto actualizado correctamente`);
     } catch (error) {
@@ -79,25 +99,7 @@ app.post('/ingitem', async (req, res) => {
     }
 });
 
-/*
-app.get('/consultaPresupuesto/:ano/:mes', async (req, res) => {
-    try {
-      const { ano, mes } = req.params;
-  
-        const Authorization = req.header("Authorization")
-        const token = Authorization.split("Bearer ")[1]
-        jwt.verify(token, "az_AZ")
-        const { email } = jwt.decode(token)
-        const ppto = await consultaPresupuesto(ano,mes)
-        res.json(ppto)
-        //res.send(`Consulta realizada correctamente`)
-                
-    } catch (error) {
-        res.status(error.code || 500).send(error)
-    }
-})
 
-*/
 
   
   app.get('/consultaPresupuesto/:ano/:mes', async (req, res) => {
@@ -133,6 +135,7 @@ app.get('/mostrarPresupuesto', async (req, res) => {
 app.post("/login", async (req, res) => {
     try {
         const { login, clave } = req.body
+        console.log(login, clave)
         await verificarCredenciales(login, clave)
         const token = jwt.sign({ login }, "az_AZ")
         res.send(token)
@@ -141,14 +144,4 @@ app.post("/login", async (req, res) => {
         res.status(error.code || 500).send(error)
     }
 })
-/*
-app.post("/usuarios", async (req, res) => {
-    try {
-        const usuario = req.body
-        await registrarUsuario(usuario)
-        res.send("Usuario creado con éxito")
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-*/
+
